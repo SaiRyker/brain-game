ceb308aef1003ec17ba204640f0d15b1d510f411
SaiRyker <daniilchernyshov0@gmail.com>
2022-09-10 19:04:00 +0300

    updates

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greetings = () => {
-  console.log("Welcome to the Brain Games!");
+  console.log('Welcome to the Brain Games!');
   const nickname = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nickname}!`);
-}
+};
 
 export default greetings;
