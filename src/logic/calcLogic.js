c8998aacd2b3e34864a6652f3f939be3caeee9cc
SaiRyker <daniilchernyshov0@gmail.com>
2022-09-17 21:26:12 +0300

    update calcLogic.js

--- src/logic/calcLogic.js
+++ src/logic/calcLogic.js
@@ -1,8 +1,9 @@
 import { getRandomInt } from "./evenLogic.js";
 import readlineSync from 'readline-sync';
+import generalProcess from "../index.js";
 
 const operators = ['*', '-', '+'];
-console.log("What is the result of the expression?");
+const instruction = "What is the result of the expression?";
 
 const calculations = (num1, num2, operator) => {
 	switch (operator) {
@@ -23,29 +24,15 @@ const calculations = (num1, num2, operator) => {
 	}
 }
 
-const main = () => {
-	let count = 0;
-	while (count != 3) {
-		const num1 = getRandomInt(0, 100);
-		const num2 = getRandomInt(0, 100);
-		const currOper = operators[getRandomInt(0, operators.length - 1)];
-		console.log(`Question: ${num1} ${currOper} ${num2}`);
-		const answer = Number(readlineSync.question('Your answer: '));
-		const result = calculations(num1, num2, currOper);
-
-		if (answer === result) {
-			console.log("Correct!");
-			count += 1;
-		} else {
-			console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}. \nLet's try again, !'`)
-			break;
-		}
-
-	}
-
-	if (count === 3) {
-		console.log("Congratulations, !")
-	}
+const mainLogic = () => {
+	const num1 = getRandomInt(0, 100);
+	const num2 = getRandomInt(0, 100);
+	const currOper = operators[getRandomInt(0, operators.length - 1)];
+	const question = `Question: ${num1} ${currOper} ${num2}`;
+	const result = String(calculations(num1, num2, currOper));
+	return [question, result];
 }
+
+const main = () => generalProcess(instruction, mainLogic);
 
 export default main;
